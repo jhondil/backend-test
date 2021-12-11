dd52b0a8b418c1dabfe004760b702cd3bc6503f8
Jhonatan Ibarra <ibarra.jhonatann@gmail.com>
2021-12-10 22:56:48 -0500

    :stethoscope:

--- swapi-back/app/mutations.py
+++ swapi-back/app/mutations.py
@@ -6,7 +6,7 @@ from .types import PlanetType
 from .utils import generic_model_mutation_process
 
 
-class AddPlanetMutation(graphene.relay.ClientIDMutation):
+class CreatePlanet(graphene.relay.ClientIDMutation):
     class Input:
         id = graphene.ID(required=False)
         name = graphene.String(required=True)
@@ -30,4 +30,6 @@ class AddPlanetMutation(graphene.relay.ClientIDMutation):
             data['id'] = from_global_id(raw_id)[1]
 
         planet = generic_model_mutation_process(**data)
-        return AddPlanetMutation(planet=planet)
+        return CreatePlanet(planet=planet)
+
+
